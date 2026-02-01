942288542d3a60c80506cec0aabba064f8996889
Felix <sitasoufelix@gmail.com>
2026-02-01 13:21:20 +0000

    Listes des declarations

---
+++ scripts/les-declarations.js
@@ -0,0 +1,232 @@
+// Declarations de l'objet DECLARATION
+const DECLARATIONS = [
+  {
+    id: "697b8208e5e27a61946708d3",
+    picture: "http://placehold.it/32x32",
+    registered: "2021-07-09T02:14:14 -00:00",
+    child: {
+      gender: "male",
+      eyeColor: "blue",
+      firstName: "Reba",
+      LastName: "Wise",
+      birthDate: "2014-04-25T06:15:01 -00:00",
+    },
+    firstParent: {
+      gender: "female",
+      eyeColor: "blue",
+      firstName: "Cleo",
+      LastName: "Cervantes",
+      birthDate: "2020-03-29T02:10:32 -00:00",
+      phone: "+1 (869) 451-2896",
+      address: "327 Kane Street, Barrelville, Minnesota, 475",
+    },
+    secondParent: {
+      gender: "female",
+      eyeColor: "blue",
+      firstName: "Latisha",
+      LastName: "Diaz",
+      birthDate: "2015-12-14T03:07:03 -00:00",
+      phone: "+1 (963) 507-3894",
+      address: "427 Chester Avenue, Nicholson, Pennsylvania, 3476",
+    },
+    company: {
+      name: "COMVEYER",
+      address: "406 Lake Avenue, Winston, Wisconsin, 7845",
+    },
+  },
+  {
+    id: "697b82084060b43cc379dc69",
+    picture: "http://placehold.it/32x32",
+    registered: "2021-11-17T12:03:46 -00:00",
+    child: {
+      gender: "female",
+      eyeColor: "green",
+      firstName: "Mayo",
+      LastName: "Sargent",
+      birthDate: "2016-02-08T11:48:02 -00:00",
+    },
+    firstParent: {
+      gender: "male",
+      eyeColor: "green",
+      firstName: "Angel",
+      LastName: "Odonnell",
+      birthDate: "2025-11-01T12:13:19 -00:00",
+      phone: "+1 (920) 512-2098",
+      address: "765 Farragut Place, Accoville, California, 9955",
+    },
+    secondParent: {
+      gender: "female",
+      eyeColor: "green",
+      firstName: "Russo",
+      LastName: "Alford",
+      birthDate: "2023-08-31T06:14:44 -00:00",
+      phone: "+1 (890) 543-2417",
+      address: "348 Suydam Street, Carbonville, Wyoming, 2298",
+    },
+    company: {
+      name: "SNORUS",
+      address: "496 Conover Street, Blandburg, Georgia, 6732",
+    },
+  },
+  {
+    id: "697b82086c8e412830c8a2f4",
+    picture: "http://placehold.it/32x32",
+    registered: "2024-06-19T12:15:48 -00:00",
+    child: {
+      gender: "male",
+      eyeColor: "blue",
+      firstName: "Carla",
+      LastName: "Webster",
+      birthDate: "2017-11-30T03:03:45 -00:00",
+    },
+    firstParent: {
+      gender: "female",
+      eyeColor: "green",
+      firstName: "Bertha",
+      LastName: "Kirk",
+      birthDate: "2019-02-09T05:05:32 -00:00",
+      phone: "+1 (990) 526-3418",
+      address: "625 Balfour Place, Soham, South Carolina, 3246",
+    },
+    secondParent: {
+      gender: "female",
+      eyeColor: "blue",
+      firstName: "Luisa",
+      LastName: "Dillon",
+      birthDate: "2017-08-18T12:36:45 -00:00",
+      phone: "+1 (975) 478-3756",
+      address: "323 Baycliff Terrace, Chloride, Louisiana, 7286",
+    },
+    company: {
+      name: "TURNABOUT",
+      address: "708 Garden Place, Needmore, Mississippi, 6014",
+    },
+  },
+  {
+    id: "697b8208d73a47260350b4b0",
+    picture: "http://placehold.it/32x32",
+    registered: "2020-09-01T06:46:56 -00:00",
+    child: {
+      gender: "female",
+      eyeColor: "green",
+      firstName: "Williams",
+      LastName: "Silva",
+      birthDate: "2024-02-25T10:14:37 -00:00",
+    },
+    firstParent: {
+      gender: "male",
+      eyeColor: "green",
+      firstName: "Burks",
+      LastName: "May",
+      birthDate: "2020-09-17T12:33:16 -00:00",
+      phone: "+1 (837) 415-3283",
+      address: "755 Amboy Street, Layhill, Colorado, 4877",
+    },
+    secondParent: {
+      gender: "male",
+      eyeColor: "green",
+      firstName: "Fulton",
+      LastName: "Koch",
+      birthDate: "2018-02-12T06:55:05 -00:00",
+      phone: "+1 (908) 541-2571",
+      address: "958 Keap Street, Blackgum, Virginia, 1608",
+    },
+    company: {
+      name: "PLASTO",
+      address: "273 Lorraine Street, Ada, Puerto Rico, 2160",
+    },
+  },
+  {
+    id: "697b8208f23cc4ca0e961d40",
+    picture: "http://placehold.it/32x32",
+    registered: "2018-04-06T03:37:02 -00:00",
+    child: {
+      gender: "male",
+      eyeColor: "brown",
+      firstName: "Jensen",
+      LastName: "Booth",
+      birthDate: "2023-04-26T11:44:51 -00:00",
+    },
+    firstParent: {
+      gender: "male",
+      eyeColor: "brown",
+      firstName: "Loraine",
+      LastName: "Gutierrez",
+      birthDate: "2018-10-20T01:33:03 -00:00",
+      phone: "+1 (885) 544-3282",
+      address: "370 Portal Street, Allamuchy, Ohio, 6219",
+    },
+    secondParent: {
+      gender: "female",
+      eyeColor: "brown",
+      firstName: "Elba",
+      LastName: "Arnold",
+      birthDate: "2016-12-24T01:02:23 -00:00",
+      phone: "+1 (845) 443-2146",
+      address: "796 Ditmars Street, Hasty, Tennessee, 5027",
+    },
+    company: {
+      name: "BALOOBA",
+      address: "669 Eastern Parkway, Neahkahnie, Washington, 4684",
+    },
+  },
+  {
+    id: "697b82084b20038373e0f2b5",
+    picture: "http://placehold.it/32x32",
+    registered: "2025-08-13T06:41:51 -00:00",
+    child: {
+      gender: "female",
+      eyeColor: "brown",
+      firstName: "Roman",
+      LastName: "Gallagher",
+      birthDate: "2018-10-06T12:49:33 -00:00",
+    },
+    firstParent: {
+      gender: "male",
+      eyeColor: "blue",
+      firstName: "Melissa",
+      LastName: "Pennington",
+      birthDate: "2018-04-21T04:03:38 -00:00",
+      phone: "+1 (829) 461-2947",
+      address: "980 Stratford Road, Kieler, Nebraska, 5364",
+    },
+    secondParent: {
+      gender: "female",
+      eyeColor: "brown",
+      firstName: "Traci",
+      LastName: "Keith",
+      birthDate: "2020-10-08T01:13:18 -00:00",
+      phone: "+1 (999) 561-3234",
+      address: "877 Billings Place, Ypsilanti, Vermont, 1641",
+    },
+    company: {
+      name: "PERKLE",
+      address:
+        "760 Tillary Street, Alamo, Federated States Of Micronesia, 3749",
+    },
+  },
+];
+
+function display() {
+  // Parcours de l'objet eclarations en utilisant une fonction fleché
+  const DECLARATION_AS_TABLE = DECLARATIONS.map((item) => {
+    const TABLE_LISTE = `
+                <tr>
+                        <td scope="row">${item.registered}</td>
+                        <td scope="row">${item.child.firstName}</td>
+                        <td scope="row">${item.child.LastName}</td>
+                        <td scope="row">
+                        <span>${item.firstParent.firstName}</span>
+                        <span>${item.firstParent.LastName}</span>
+                        </td>
+                        <td scope="row">
+                        <span>${item.secondParent.firstName}</span>
+                        <span>${item.secondParent.LastName}</span>
+                        </td>
+                    </tr>
+        `;
+    return TABLE_LISTE;
+  });
+  document.getElementById("table-container").innerHTML =
+    DECLARATION_AS_TABLE.join(" ");
+}
